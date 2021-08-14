1f99fba3951fd766907c3bac58fa26d407df5ed1
rajik470 <dmitryrajik@gmail.com>
2021-08-14 20:41:48 +0300

    resizeable

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
 
+
+int numberOfPoints = 1000000;
+
+
 sf::Vector2f calculatePosition(sf::Vector2f a, sf::Vector2f b)
 {
   sf::Vector2f position((a.x + b.x) / 2, (a.y + b.y) / 2); 
@@ -20,31 +24,34 @@ sf::VertexArray getPoints(sf::VertexArray points, int numberOfPoints)
   return newPoints;
 }
 
+void drawTriangle(sf::RenderWindow& window)
+{
+    sf::VertexArray startPoints(sf::Points, 3);
+    startPoints[0].position = sf::Vector2f(10.f, 240.f);
+    startPoints[1].position = sf::Vector2f(240.f, 10.f);
+    startPoints[2].position = sf::Vector2f(480.f, 240.f);
+
+    window.clear(sf::Color::Black);
+    window.draw(startPoints);
+    sf::VertexArray points(sf::Points, numberOfPoints);
+    points = getPoints(startPoints, numberOfPoints);
+    window.draw(points);
+    window.display();
+}
 int main()
 {
   srand(time(0));
-  sf::RenderWindow window(sf::VideoMode(500, 500), "fractal");
+  sf::RenderWindow window(sf::VideoMode(500, 250), "fractal");
  
-  int numberOfPoints;
-  std::cin >> numberOfPoints;
+//  std::cin >> numberOfPoints;
 
   sf::VertexArray startPoints(sf::Points, 3);
   startPoints[0].position = sf::Vector2f(10.f, 240.f);
   startPoints[1].position = sf::Vector2f(240.f, 10.f);
   startPoints[2].position = sf::Vector2f(480.f, 240.f);
 
-  sf::CircleShape shape(2.f);
-  shape.setFillColor(sf::Color::Red);
-//  window.draw(shape, point1);
-//  window.draw(shape, point2);
-//  window.draw(shape, point3);
-  window.clear(sf::Color::Black);
-  window.draw(startPoints);
-  sf::VertexArray points(sf::Points, numberOfPoints);
-  points = getPoints(startPoints, numberOfPoints);
-  window.draw(points);
-  window.display();
-
+  window.setFramerateLimit(15);
+  drawTriangle(window);
   while (window.isOpen())
   {
     sf::Event event;
@@ -53,6 +60,7 @@ int main()
       if (event.type == sf::Event::Closed)
         window.close();
     }
+    drawTriangle(window);
   }
   return 0;
 }
